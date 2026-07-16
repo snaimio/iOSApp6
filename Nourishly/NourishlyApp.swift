f0352d9f98da70005dfd7b784b0bc54cab9da02e
Sheikh Naim <naimbgd@gmail.com>
2026-07-16 08:41:45 -0400

    Initial Commit

---
+++ Nourishly/NourishlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NourishlyApp.swift
+//  Nourishly
+//
+//  Created by Sheikh Naim on 2026-07-16.
+//
+
+import SwiftUI
+
+@main
+struct NourishlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
